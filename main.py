3c79023fe40094e548951d8f1a6825305a9d2a32
Evil_Sensei <sajidmahmudsayan909@gmail.com>
2025-05-13 14:10:11 +0600

    main.py maken for your buisness ai help

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 a = 3492
 print(f"Your bill is {a}$")
 b = int(input("Give your bill: "))
-if b > a:
+
+if a == b:
     print("Thanks for your patience")
-elif a == b:
-    print("Thanks for your patience")
+elif b > a:
+    more2  = b - a
+    print(f"You payed more than bill take your {more2}$ ")
 else:
     more = a-b
     print(f"You need to pay more {more}$")
